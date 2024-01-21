20be142098679883e43a03d23bd481e7cb127c36
Soumya Keshri <soumyakeshri8298@gmail.com>
2024-01-21 20:28:00 +0530

    change in index.html

---
+++ ss1.js
@@ -0,0 +1,6 @@
+// ss1.js
+
+function toggleTimetable() {
+    var timetableContainer = document.getElementById("timetableContainer");
+    timetableContainer.style.display = timetableContainer.style.display === "none" ? "block" : "none";
+}
