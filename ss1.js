a41feec874cf5abb8f1c81662dcdbf4df6ad8a9b
Shreya kumari <shreya@Shreyas-MacBook-Pro-2.local>
2024-01-22 20:14:15 +0530

    Changes in  index.html

--- ss1.js
+++
@@ -1,6 +0,0 @@
-// ss1.js
-
-function toggleTimetable() {
-    var timetableContainer = document.getElementById("timetableContainer");
-    timetableContainer.style.display = timetableContainer.style.display === "none" ? "block" : "none";
-}
